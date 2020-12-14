85748c74e87e10f1e79f11331a4abe25ffa1b371
martinagabrielli <martina.gabrielli@yahoo.it>
2020-12-14 14:20:59 +0000

    increase counter every time colour is clicked

--- src/Main.js
+++ src/Main.js
@@ -1,31 +1,25 @@
-import React, { useState } from 'react'
-
+import React, { useState, useEffect } from 'react'
 
 function Main() {
-    const hexDigits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
-
-    const [storage, setStorage] = useState(0)
+    const [counter, setCounter] = useState(0)
     const [colour, setColour] = useState('#FFFFFF')
-
-    const increment = (prev) => {
-        setStorage(prev => prev + 1)
-    }
     
     const generateCombination = () => {
+        const hexDigits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
         let newColour = ''
         for(let i = 1; i <= 6; i++) {
             newColour += hexDigits[Math.floor(Math.random() * hexDigits.length)]
         }
         newColour = `#${newColour}`
         setColour(newColour)
+        setCounter(prev => prev + 1)
     }
 
     return (
         <main style={{background: colour}}>
             <section>
                 <h1>Counter</h1>
-                <div id="display">{storage}</div>
-                <button className="counter" onClick={increment}>Click</button>
+                <div id="display">{counter}</div>
             </section>
             <section>
                 <button className="change-colour" onClick={generateCombination}>Change Colour</button>
