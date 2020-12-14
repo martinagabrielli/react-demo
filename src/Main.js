306ac5a91f6bc482e55a1bcf4a9d4b8263c4f736
martinagabrielli <martina.gabrielli@yahoo.it>
2020-12-14 15:14:45 +0000

    tested useEffect

--- src/Main.js
+++ src/Main.js
@@ -3,6 +3,10 @@ import React, { useState, useEffect } from 'react'
 function Main() {
     const [counter, setCounter] = useState(0)
     const [colour, setColour] = useState('#FFFFFF')
+
+    useEffect(() => {
+        console.log('hello!')
+    }, [])
     
     const generateCombination = () => {
         const hexDigits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
