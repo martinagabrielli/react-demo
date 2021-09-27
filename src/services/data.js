58ab57d7c4968d9dc1b17b9edde0e93c6d449939
martinagabrielli <martina.gabrielli@yahoo.it>
2021-09-27 22:17:40 +0100

    Add data.js

---
+++ src/services/data.js
@@ -0,0 +1,277 @@
+const data = [
+  {
+    "_id": "615204f229382d957dc37cec",
+    "index": 0,
+    "guid": "cfae616e-1a3a-4d23-bf66-ba4fad163d74",
+    "isActive": false,
+    "balance": "$1,804.80",
+    "picture": "http://placehold.it/32x32",
+    "age": 23,
+    "eyeColor": "blue",
+    "name": "Patsy Marks",
+    "gender": "female",
+    "company": "TWIIST",
+    "email": "patsymarks@twiist.com",
+    "phone": "+1 (954) 591-2389",
+    "address": "607 Berriman Street, Roosevelt, Minnesota, 7766",
+    "about": "Eiusmod et laboris reprehenderit aliqua laboris. Culpa reprehenderit ut consequat cupidatat sit esse pariatur do labore. Ea commodo irure ex sint laborum veniam aliquip anim reprehenderit officia nostrud excepteur nisi velit. Incididunt nulla Lorem ullamco qui. Adipisicing ea nulla aliqua velit cupidatat magna officia quis.\r\n",
+    "registered": "2018-12-10T03:41:21 -00:00",
+    "latitude": -56.590334,
+    "longitude": -164.685338,
+    "tags": [
+      "pariatur",
+      "cupidatat",
+      "sint",
+      "eiusmod",
+      "sit",
+      "ea",
+      "enim"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Foley Solis"
+      },
+      {
+        "id": 1,
+        "name": "Vega Wolf"
+      },
+      {
+        "id": 2,
+        "name": "Angela Coffey"
+      }
+    ],
+    "greeting": "Hello, Patsy Marks! You have 7 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "615204f25c32163c06bd4fa0",
+    "index": 1,
+    "guid": "f2c30fda-b5f8-4b86-be72-7bf9509fddf0",
+    "isActive": true,
+    "balance": "$1,538.24",
+    "picture": "http://placehold.it/32x32",
+    "age": 31,
+    "eyeColor": "blue",
+    "name": "Cecile Davis",
+    "gender": "female",
+    "company": "NORALI",
+    "email": "ceciledavis@norali.com",
+    "phone": "+1 (882) 586-2220",
+    "address": "356 Church Lane, Sutton, New Jersey, 1439",
+    "about": "Qui proident pariatur eiusmod nisi commodo dolore in anim adipisicing id excepteur est velit consequat. Amet Lorem irure sit aliqua voluptate id nostrud. Occaecat reprehenderit cillum dolore qui occaecat et eu quis eiusmod esse. Ea duis sunt cillum in dolor. Cupidatat commodo irure Lorem consequat minim ullamco esse cupidatat. Magna nostrud occaecat anim aliqua qui commodo fugiat dolor qui tempor aute consectetur voluptate. Veniam est minim nulla mollit reprehenderit non irure voluptate non aute eu commodo nulla adipisicing.\r\n",
+    "registered": "2016-04-23T06:22:18 -01:00",
+    "latitude": 36.359724,
+    "longitude": -4.926102,
+    "tags": [
+      "id",
+      "ullamco",
+      "fugiat",
+      "eu",
+      "cupidatat",
+      "non",
+      "amet"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Marcella Burns"
+      },
+      {
+        "id": 1,
+        "name": "Mullins Simmons"
+      },
+      {
+        "id": 2,
+        "name": "Valarie Acosta"
+      }
+    ],
+    "greeting": "Hello, Cecile Davis! You have 4 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "615204f2a81c7d5d0cb109fb",
+    "index": 2,
+    "guid": "f909e082-fab1-4248-918d-7ef9e49b83b1",
+    "isActive": false,
+    "balance": "$1,759.23",
+    "picture": "http://placehold.it/32x32",
+    "age": 35,
+    "eyeColor": "brown",
+    "name": "Alston Cole",
+    "gender": "male",
+    "company": "PLASMOS",
+    "email": "alstoncole@plasmos.com",
+    "phone": "+1 (968) 547-2373",
+    "address": "335 Clymer Street, Wright, Georgia, 9510",
+    "about": "Exercitation veniam do adipisicing magna consequat veniam. Pariatur ullamco est excepteur pariatur occaecat reprehenderit. Consequat laborum cupidatat elit aute veniam laborum quis quis velit enim sint eiusmod adipisicing. Cupidatat consequat non velit minim fugiat.\r\n",
+    "registered": "2014-09-16T11:20:05 -01:00",
+    "latitude": -61.313105,
+    "longitude": 148.653038,
+    "tags": [
+      "do",
+      "reprehenderit",
+      "ut",
+      "eu",
+      "magna",
+      "voluptate",
+      "eiusmod"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Pope Berry"
+      },
+      {
+        "id": 1,
+        "name": "Nolan Patterson"
+      },
+      {
+        "id": 2,
+        "name": "Alexis Bentley"
+      }
+    ],
+    "greeting": "Hello, Alston Cole! You have 4 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "615204f2b26e6687ab8ac790",
+    "index": 3,
+    "guid": "aee2e78d-c244-437f-a5c7-58f384387cae",
+    "isActive": false,
+    "balance": "$1,540.41",
+    "picture": "http://placehold.it/32x32",
+    "age": 21,
+    "eyeColor": "green",
+    "name": "Bishop Atkinson",
+    "gender": "male",
+    "company": "GROK",
+    "email": "bishopatkinson@grok.com",
+    "phone": "+1 (804) 490-2154",
+    "address": "806 Applegate Court, Babb, North Dakota, 893",
+    "about": "Enim voluptate adipisicing ad incididunt. Culpa dolore aliqua ad laborum. Labore qui consequat non non ut id adipisicing consectetur mollit eu nostrud.\r\n",
+    "registered": "2016-08-24T03:03:38 -01:00",
+    "latitude": -48.108427,
+    "longitude": -72.317715,
+    "tags": [
+      "non",
+      "esse",
+      "reprehenderit",
+      "mollit",
+      "eiusmod",
+      "qui",
+      "labore"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Rosa Fernandez"
+      },
+      {
+        "id": 1,
+        "name": "Penny Kelley"
+      },
+      {
+        "id": 2,
+        "name": "Anne Schneider"
+      }
+    ],
+    "greeting": "Hello, Bishop Atkinson! You have 9 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "615204f2b6508ebbc963eeb8",
+    "index": 4,
+    "guid": "16ad76fd-83e1-476e-9ef0-567d41d013e3",
+    "isActive": false,
+    "balance": "$1,304.23",
+    "picture": "http://placehold.it/32x32",
+    "age": 33,
+    "eyeColor": "green",
+    "name": "Shannon Bradford",
+    "gender": "male",
+    "company": "EXOBLUE",
+    "email": "shannonbradford@exoblue.com",
+    "phone": "+1 (930) 552-3776",
+    "address": "701 Doone Court, Spokane, Texas, 6902",
+    "about": "Proident ex est tempor ullamco eiusmod irure elit commodo voluptate laboris. Sit reprehenderit minim consectetur qui fugiat amet pariatur. Pariatur ex nulla sunt incididunt labore dolor tempor mollit amet duis culpa aliqua cillum sint.\r\n",
+    "registered": "2020-11-29T02:58:32 -00:00",
+    "latitude": -85.996767,
+    "longitude": -120.040837,
+    "tags": [
+      "adipisicing",
+      "cupidatat",
+      "aliquip",
+      "duis",
+      "dolore",
+      "occaecat",
+      "sunt"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Mitzi Lucas"
+      },
+      {
+        "id": 1,
+        "name": "Leah Alston"
+      },
+      {
+        "id": 2,
+        "name": "Fitzpatrick Dawson"
+      }
+    ],
+    "greeting": "Hello, Shannon Bradford! You have 5 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "615204f2acfc819003eb0400",
+    "index": 5,
+    "guid": "48043f84-8495-4398-887b-fec978e04223",
+    "isActive": true,
+    "balance": "$2,409.91",
+    "picture": "http://placehold.it/32x32",
+    "age": 32,
+    "eyeColor": "green",
+    "name": "Herring Maldonado",
+    "gender": "male",
+    "company": "CORPORANA",
+    "email": "herringmaldonado@corporana.com",
+    "phone": "+1 (845) 439-2672",
+    "address": "755 Montana Place, Utting, Palau, 2225",
+    "about": "Laborum deserunt ad amet culpa irure commodo. Ullamco ex elit in elit aliqua duis et id consectetur elit. Consectetur ut nulla ipsum mollit non sunt sunt labore excepteur nisi voluptate irure magna id.\r\n",
+    "registered": "2019-12-01T03:19:57 -00:00",
+    "latitude": 33.155388,
+    "longitude": 27.810945,
+    "tags": [
+      "quis",
+      "quis",
+      "pariatur",
+      "esse",
+      "qui",
+      "nostrud",
+      "anim"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Walls Mendez"
+      },
+      {
+        "id": 1,
+        "name": "Ophelia Schultz"
+      },
+      {
+        "id": 2,
+        "name": "Gail Odom"
+      }
+    ],
+    "greeting": "Hello, Herring Maldonado! You have 4 unread messages.",
+    "favoriteFruit": "apple"
+  }
+]
+
+export function getData() {
+    console.log('working')
+    return data
+}
